60925204f5cc2b1294dfa75ff78aca00c07fef22
HOME\JEE_MINHEE <jee_minhee@elandinnople.com>
2026-03-23 08:59:34 +0900

    TLS 에러 제거

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -8,6 +8,17 @@ if (!connectionString) {
   throw new Error("DATABASE_URL is not set");
 }
 
-const adapter = new PrismaPg({ connectionString });
+const databaseUrl = new URL(connectionString);
+
+// RDS 접속은 코드에서 SSL 옵션을 제어하므로 URL의 sslmode는 제거한다.
+databaseUrl.searchParams.delete("sslmode");
+databaseUrl.searchParams.delete("uselibpqcompat");
+
+const adapter = new PrismaPg({
+  connectionString: databaseUrl.toString(),
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 export const prisma = new PrismaClient({ adapter });
